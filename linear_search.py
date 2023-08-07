883e11b12a07e63dfc8ff6c659142ddb70f17dde
Ayan Shaikh <ayansk11@Ayans-MacBook-Air.local>
2023-08-08 01:44:28 +0530

    final push for youtube

--- linear_search.py
+++ linear_search.py
@@ -2,7 +2,7 @@
 #   @author [Ayan Shaikh]
 #   @email [ayansk152@gmail.com]
 #   @create date 2023-06-21 22:08:33
-#   @modify date 2023-06-29 20:23:48
+#   @modify date 2023-08-08 01:43:31
 #   @desc [Implementing Linear Search in Python]
 
 
@@ -17,9 +17,11 @@
 #   Worst Case	            O(n)
 
 
-import random
-import time
-import numpy as np
+
+import random                                   # importing built-in random module. The random module provides functions to generate random numbers and perform random operations. 
+import time                                     # importing built-in time module which provides various functions related to time and timing. It can be used for tasks like measuring the time taken by a piece of code to execute or introducing delays in your program.
+import numpy as np                              # importing the third-party library numpy and giving it the alias np. numpy is a powerful library for numerical computations in Python. It provides support for working with large, multi-dimensional arrays and matrices, along with a wide range of mathematical functions to operate on these arrays efficiently.
+
 
 print("\nCreating an array of random numbers\n") 
 
@@ -30,7 +32,7 @@ print("\n")
 
 counter = 0
 
-def linearsearch(search_for, arr, counter):     # function that will perform linear search (iteratively)
+def linearsearch(search_for, arr, counter):     # Defining the function that will perform linear search (iteratively)
 
     for i in range(len(arr)):
 
@@ -45,10 +47,10 @@ def linearsearch(search_for, arr, counter):     # function that will perform lin
 search_for = int(input("Enter the number you want to search: "))
         
 tic = time.time()  # capture start time
-search_result = linearsearch(search_for, arr, counter)
+search_result = linearsearch(search_for, arr, counter)          # Calling the linearsearch function that we defined above 
 toc = time.time()  # capture end time
 
-if search_result[0] == -1:
+if search_result[0] == -1:                                      # printing the result
     
     print("\n"+"*****"*12)
     print(f"\nNumber {search_for} not found in the list\n")
